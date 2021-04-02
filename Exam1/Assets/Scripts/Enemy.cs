9cc83640073db591d12c997ac83d83a3a9d8ced9
whdbcks111 <whdbcks111@gmail.com>
2021-04-02 17:17:12 +0900

    0402

---
+++ Exam1/Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int directionVector = 1;
+    public int speed = 7;
+    private SpriteRenderer renderer;
+
+    public void SetDirectionVector(int direction)
+    {
+        directionVector = direction;
+    }
+
+    void Awake()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x += directionVector * speed * Time.deltaTime;
+        transform.position = pos;
+
+        if (pos.x < -13 || pos.x > 13) Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Exam1/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public Enemy enemy;
+    public GameObject enemyGroup;
+    private bool isPlaying = true;
+
+    public void SpawnerOff()
+    {
+        isPlaying = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlaying)
+        {
+            float spawnPer = Random.Range(0, 1001) / 10.0f;
+            if (spawnPer < 2.0)
+            {
+                bool isLeft = Random.Range(0, 2) == 0;
+                Enemy e = Instantiate(enemy);
+                float posY = Random.Range(-5.2f, 5.2f);
+                e.transform.position = new Vector3(isLeft ? -9 : 9, posY, 0);
+                e.GetComponent<SpriteRenderer>().flipX = !isLeft;
+                e.SetDirectionVector(isLeft ? 1 : -1);
+            }
+        }
+    }
+}
